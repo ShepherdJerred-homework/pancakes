d8db135aaae18239678c06753e2f1a072f4e4059
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-11-09 14:11:18 -0600

    finish impl

--- src/pancakes.java
+++ src/pancakes.java
@@ -4,10 +4,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class pancakes {
     public static void main(String[] args) throws FileNotFoundException {
@@ -16,7 +13,7 @@ public class pancakes {
         Scanner scanner = new Scanner(inputFile);
         PrintWriter printWriter = new PrintWriter(outputFile);
 
-        List<Stack> stacks = new ArrayList<>();
+        List<PancakeStack> pancakeStacks = new ArrayList<>();
 
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
@@ -26,83 +23,168 @@ public class pancakes {
             }
 
             char[] c = line.toCharArray();
-            Stack s = Stack.fromChars(c);
-            stacks.add(s);
+            PancakeStack s = new PancakeStack(c);
+            pancakeStacks.add(s);
         }
-        solve(stacks);
-    }
-
-    static void solve(List<Stack> stacks) {
-        stacks.forEach(stack -> {
-            int min = findMinFlips(stack);
-            System.out.print(min);
+        List<Integer> l = solve(pancakeStacks);
+        l.forEach(output -> {
+            printWriter.println(output);
+            System.out.println(output);
         });
+        printWriter.close();
     }
 
-    static int findMinFlips(Stack stack) {
-        // TODO
-        return 0;
+    static List<Integer> solve(List<PancakeStack> pancakeStacks) {
+        List<Integer> l = new ArrayList<>();
+        pancakeStacks.forEach(pancakeStack -> {
+            System.out.println("Solving " + pancakeStack);
+            int min = pancakeStack.solve();
+            l.add(min);
+        });
+        return l;
     }
 
-    static class Stack {
-        Pancake[] pancakes;
-        int size;
+    static class QueuePair {
+        PancakeStack pancakeStack;
+        int flipIndex;
+        int numFlips;
 
-        Stack(int size) {
-            pancakes = new Pancake[size];
-            this.size = size;
+        @Override
+        public String toString() {
+            return "QueuePair{" +
+                    "pancakeStack=" + pancakeStack +
+                    ", flipIndex=" + flipIndex +
+                    ", numFlips=" + numFlips +
+                    '}';
         }
 
-        public Stack(Pancake[] pancakes, int size) {
+        public QueuePair(PancakeStack pancakeStack, int flipIndex, int numFlips) {
+            this.pancakeStack = pancakeStack;
+            this.flipIndex = flipIndex;
+            this.numFlips = numFlips;
+        }
+    }
+
+    static class PancakeStack {
+        final Pancake[] pancakes;
+        final int size;
+
+        public PancakeStack(Pancake[] pancakes, int size) {
             this.pancakes = pancakes;
             this.size = size;
         }
 
-        static Stack fromChars(char[] c) {
+        public PancakeStack(char[] c) {
             int size = c.length;
             Pancake[] pancakes = new Pancake[size];
-            Stack s = new Stack(size);
             for (int i = 0; i < c.length; i++) {
                 pancakes[i] = new Pancake(c[i]);
             }
-            s.pancakes = pancakes;
-            return s;
+            this.pancakes = pancakes;
+            this.size = c.length;
         }
 
-        public Stack flip(int start) {
+        public int solve() {
+            if (isSorted()) {
+                return 0;
+            }
 
-            Pancake[] pancakes = Arrays.copyOf(this.pancakes, size);
-            int elementsBetweenStartAndEnd = size - start;
-            if (elementsBetweenStartAndEnd == 0) {
-                pancakes[start].flip();
-            } else {
-                for (int i = start; i < elementsBetweenStartAndEnd / 2; i++) {
-                    Pancake l = pancakes[i];
-                    Pancake r = pancakes[size - 1 - i];
-                    l.flip();
-                    r.flip();
-                    pancakes[i] = r;
-                    pancakes[size - 1 - i] = l;
+            Set<PancakeStack> set = new HashSet<>();
+            Queue<QueuePair> queue = new LinkedList<>();
+
+            set.add(this);
+            for (int i = 0; i < size; i++) {
+                queue.add(new QueuePair(this, i + 1, 0));
+            }
+
+            while (!queue.isEmpty()) {
+                QueuePair qp = queue.poll();
+                PancakeStack original = qp.pancakeStack;
+
+//                System.out.println(qp);
+                PancakeStack flippedPancakeStack = original.flip(qp.flipIndex);
+                if (set.contains(flippedPancakeStack)) {
+                    System.out.println("Pruning");
+                    continue;
+                } else {
+                    set.add(flippedPancakeStack);
                 }
-                if (elementsBetweenStartAndEnd % 2 != 0) {
-                    pancakes[start + (elementsBetweenStartAndEnd / 2)].flip();
+                if (flippedPancakeStack.isSorted()) {
+                    return qp.numFlips + 1;
+                } else {
+                    for (int i = 0; i < flippedPancakeStack.size; i++) {
+                        queue.add(new QueuePair(flippedPancakeStack, i + 1, qp.numFlips + 1));
+                    }
                 }
             }
 
-            return new Stack(pancakes, size);
+            return -1;
+        }
+
+        public PancakeStack flip(int num) {
+            Stack<Pancake> s1 = new Stack<>();
+            Stack<Pancake> s2 = new Stack<>();
+            Stack<Pancake> s3 = new Stack<>();
+
+            Pancake[] pancakes = new Pancake[size];
+            for (int i = 0; i < size; i++) {
+                Pancake p = new Pancake(this.pancakes[i]);
+                pancakes[i] = p;
+                s1.push(p);
+            }
+
+            for (int i = 0; i < num; i++) {
+                s2.push(s1.pop());
+            }
+
+            for (int i = 0; i < num; i++) {
+                s3.push(s2.pop());
+            }
+
+            for (int i = 0; i < num; i++) {
+                Pancake p = s3.pop();
+                p.flip();
+                s1.push(p);
+            }
+
+            for (int i = 0; i < size; i++) {
+                s3.push(s1.pop());
+            }
+
+            for (int i = 0; i < size; i++) {
+                pancakes[i] = s3.pop();
+            }
+
+            return new PancakeStack(pancakes, size);
         }
 
         public boolean isSorted() {
             int lastSize = pancakes[0].size;
             for (int i = 0; i < size; i++) {
                 Pancake p = pancakes[i];
-                if (p.size < lastSize) {
+                if (p.size < lastSize || p.burntSideOrientation == Pancake.Orientation.UP) {
                     return false;
                 } else {
                     lastSize = p.size;
                 }
             }
             return true;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            PancakeStack that = (PancakeStack) o;
+            return size == that.size &&
+                    Arrays.equals(pancakes, that.pancakes);
+        }
+
+        @Override
+        public int hashCode() {
+            int result = Objects.hash(size);
+            result = 31 * result + Arrays.hashCode(pancakes);
+            return result;
         }
 
         @Override
@@ -120,6 +202,11 @@ public class pancakes {
             this.burntSideOrientation = Character.isUpperCase(c) ? Orientation.DOWN : Orientation.UP;
         }
 
+        public Pancake(Pancake p) {
+            this.size = p.size;
+            this.burntSideOrientation = p.burntSideOrientation;
+        }
+
         void flip() {
             if (burntSideOrientation == Orientation.DOWN) {
                 burntSideOrientation = Orientation.UP;
@@ -132,6 +219,8 @@ public class pancakes {
             char c = (char) (size + 97);
             if (burntSideOrientation == Orientation.DOWN) {
                 c = Character.toUpperCase(c);
+            } else {
+                c = Character.toLowerCase(c);
             }
             return c;
         }
@@ -144,6 +233,20 @@ public class pancakes {
         @Override
         public String toString() {
             return String.valueOf(toChar());
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Pancake pancake = (Pancake) o;
+            return size == pancake.size &&
+                    burntSideOrientation == pancake.burntSideOrientation;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(size, burntSideOrientation);
         }
     }
 }
--- test/pancakesTest.java
+++ test/pancakesTest.java
@@ -4,62 +4,99 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class pancakesTest {
     @Test
+    public void testFlipSequence() {
+        pancakes.PancakeStack pancakeStack = new pancakes.PancakeStack(new char[]{'a', 'b', 'c'});
+
+        pancakes.PancakeStack flippedPancakeStack = pancakeStack.flip(2);
+        assertEquals("[a, C, B]", flippedPancakeStack.toString());
+
+        flippedPancakeStack = flippedPancakeStack.flip(3);
+        assertEquals("[b, c, A]", flippedPancakeStack.toString());
+
+        flippedPancakeStack = flippedPancakeStack.flip(2);
+        assertEquals("[b, a, C]", flippedPancakeStack.toString());
+
+        flippedPancakeStack = flippedPancakeStack.flip(3);
+        assertEquals("[c, A, B]", flippedPancakeStack.toString());
+
+        flippedPancakeStack = flippedPancakeStack.flip(2);
+        assertEquals("[c, b, a]", flippedPancakeStack.toString());
+
+        flippedPancakeStack = flippedPancakeStack.flip(3);
+        assertEquals("[A, B, C]", flippedPancakeStack.toString());
+
+        assertTrue(flippedPancakeStack.isSorted());
+    }
+
+    @Test
+    public void testFlipWithOddNumberOfPancakesStartingInMiddle() {
+        pancakes.PancakeStack pancakeStack = new pancakes.PancakeStack(new char[]{'a', 'b', 'c'});
+        pancakes.PancakeStack flippedPancakeStack = pancakeStack.flip(2);
+        assertEquals("[a, C, B]", flippedPancakeStack.toString());
+    }
+
+    @Test
     public void testFlipWithOddNumberOfPancakesStartingAtBeginning() {
-        pancakes.Stack stack = pancakes.Stack.fromChars(new char[]{'a', 'b', 'c'});
-        pancakes.Stack flippedStack = stack.flip(0);
-        assertEquals("[C, B, A]", flippedStack.toString());
+        pancakes.PancakeStack pancakeStack = new pancakes.PancakeStack(new char[]{'a', 'b', 'c'});
+        pancakes.PancakeStack flippedPancakeStack = pancakeStack.flip(3);
+        assertEquals("[C, B, A]", flippedPancakeStack.toString());
     }
 
     @Test
     public void testFlipWithEvenNumberOfPancakesStartingAtBeginning() {
-        pancakes.Stack stack = pancakes.Stack.fromChars(new char[]{'a', 'b', 'c', 'd'});
-        pancakes.Stack flippedStack = stack.flip(0);
-        assertEquals("[D, C, B, A]", flippedStack.toString());
+        pancakes.PancakeStack pancakeStack = new pancakes.PancakeStack(new char[]{'a', 'b', 'c', 'd'});
+        pancakes.PancakeStack flippedPancakeStack = pancakeStack.flip(4);
+        assertEquals("[D, C, B, A]", flippedPancakeStack.toString());
     }
 
     @Test
     public void testFlipWithOddNumberOfPancakesStartingAtEnd() {
-        pancakes.Stack stack = pancakes.Stack.fromChars(new char[]{'a', 'b', 'c'});
-        pancakes.Stack flippedStack = stack.flip(2);
-        assertEquals("[a, b, C]", flippedStack.toString());
+        pancakes.PancakeStack pancakeStack = new pancakes.PancakeStack(new char[]{'a', 'b', 'c'});
+        pancakes.PancakeStack flippedPancakeStack = pancakeStack.flip(1);
+        assertEquals("[a, b, C]", flippedPancakeStack.toString());
     }
 
     @Test
     public void testFlipWithEvenNumberOfPancakesStartingAtEnd() {
-        pancakes.Stack stack = pancakes.Stack.fromChars(new char[]{'a', 'b', 'c', 'd'});
-        pancakes.Stack flippedStack = stack.flip(3);
-        assertEquals("[a, b, c, D]", flippedStack.toString());
+        pancakes.PancakeStack pancakeStack = new pancakes.PancakeStack(new char[]{'a', 'b', 'c', 'd'});
+        pancakes.PancakeStack flippedPancakeStack = pancakeStack.flip(1);
+        assertEquals("[a, b, c, D]", flippedPancakeStack.toString());
     }
 
     @Test
     public void testFlipWithOnePancake() {
-        pancakes.Stack stack = pancakes.Stack.fromChars(new char[]{'a'});
-        pancakes.Stack flippedStack = stack.flip(0);
-        assertEquals("[A]", flippedStack.toString());
+        pancakes.PancakeStack pancakeStack = new pancakes.PancakeStack(new char[]{'a'});
+        pancakes.PancakeStack flippedPancakeStack = pancakeStack.flip(1);
+        assertEquals("[A]", flippedPancakeStack.toString());
     }
 
     @Test
     public void testIsSortedReturnsFalseWhenListIsNotSorted() {
-        pancakes.Stack stack = pancakes.Stack.fromChars(new char[]{'a', 'c', 'b'});
-        assertFalse(stack.isSorted());
+        pancakes.PancakeStack pancakeStack = new pancakes.PancakeStack(new char[]{'a', 'c', 'b'});
+        assertFalse(pancakeStack.isSorted());
 
-        stack = pancakes.Stack.fromChars(new char[]{'c', 'b', 'a'});
-        assertFalse(stack.isSorted());
+        pancakeStack = new pancakes.PancakeStack(new char[]{'c', 'b', 'a'});
+        assertFalse(pancakeStack.isSorted());
 
-        stack = pancakes.Stack.fromChars(new char[]{'b', 'a'});
-        assertFalse(stack.isSorted());
+        pancakeStack = new pancakes.PancakeStack(new char[]{'b', 'a'});
+        assertFalse(pancakeStack.isSorted());
+    }
+
+    @Test public void testIsSortedReturnsFalseWhenPancakesAreNotUpsideDown() {
+        pancakes.PancakeStack pancakeStack = new pancakes.PancakeStack(new char[]{'a', 'B', 'C'});
+        assertFalse(pancakeStack.isSorted());
     }
 
     @Test
     public void testIsSortedReturnsTrueWhenListIsSorted() {
-        pancakes.Stack stack = pancakes.Stack.fromChars(new char[]{'a', 'b', 'c'});
-        assertTrue(stack.isSorted());
+        pancakes.PancakeStack pancakeStack = new pancakes.PancakeStack(new char[]{'A', 'B', 'C'});
+        assertTrue(pancakeStack.isSorted());
 
-        stack = pancakes.Stack.fromChars(new char[]{'a', 'b'});
-        assertTrue(stack.isSorted());
+        pancakeStack = new pancakes.PancakeStack(new char[]{'A', 'B'});
+        assertTrue(pancakeStack.isSorted());
 
-        stack = pancakes.Stack.fromChars(new char[]{'a'});
-        assertTrue(stack.isSorted());
+        pancakeStack = new pancakes.PancakeStack(new char[]{'C'});
+        assertTrue(pancakeStack.isSorted());
     }
 
     @Test
@@ -79,5 +116,16 @@ class pancakesTest {
         p = new pancakes.Pancake('Z');
         assertEquals(25, p.size);
         assertEquals(pancakes.Pancake.Orientation.DOWN, p.burntSideOrientation);
+    }
+
+    @Test
+    public void testSolve() {
+        pancakes.PancakeStack pancakeStack = new pancakes.PancakeStack(new char[]{'a', 'b', 'c'});
+        assertEquals(6, pancakeStack.solve());
+    }
+
+    @Test
+    public void testGivenCases() {
+
     }
 }
